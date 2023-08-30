d6a533f3580081caba27f1a05fa9631cd722b7d8
Jochen <jochen.rath@hotmail.de>
2023-08-30 17:30:38 +0200

    Datenauswertung um Streifendiagramm erweitert

--- Funktionen/funktionenErzeugeDatenDiagramme.py
+++ Funktionen/funktionenErzeugeDatenDiagramme.py
@@ -8,10 +8,13 @@ import random
 def datenliste():
     daten={'Lob von Lehrern am Tag':['Gut', 'Schön', 'Toll', 'andere',120]}
     daten['Autos pro Stunde gezählt'] = ['VW', 'Audi', 'Fiat', 'Mercedes', 'Honda','Dacia',400]
+    daten['Seitenaufrufe aus Ländern pro Minute'] = ['Amerika', 'China', 'Deutschland', 'Frankreich', 'Japan', 'Schweiz', 200]
+    daten['Schülergrößen an der Schule'] = [F'{random.randint(150,190)} cm' for i in range(6)] + [300]
+    daten['Alter von Einwohnern in einer Stadt'] = [F'{random.randint(0, 100)} Jahre' for i in range(6)] + [200]
 #    daten['T-Shirt Farben gewaschen']=['Schwarz','Orange','Gelb','Blau','Grün',300]
     return daten
 
-def erzeugeSaeulenKreisDiagramm(typ='ZeichnenUndBerechnen',mitText=True):
+def erzeugeSaeulenStreifenKreisDiagramm(typ='ZeichnenUndBerechnen',mitText=True,streifen=False):
     #Diese Funktion erzeugt eine Tabelle,zu der SuS ein Säulen- und Kreisdiagramm erzeugen sollen.
     auswahl=random.choice(list(datenliste().keys()))
     werte=[datenliste()[auswahl][0:-1]]
@@ -21,23 +24,23 @@ def erzeugeSaeulenKreisDiagramm(typ='ZeichnenUndBerechnen',mitText=True):
         if typ=='zuordnen':
             afg = afg + [F'Ordne die Daten den Diagrammen zu.\\\\']
             afg = afg + [F'Inhalt der Daten: {auswahl}\\\\']
-            afg = afg + saeulenDiagrammeZeichnen(werte=werte,typ=typ,titel=auswahl)
+            afg = afg + saeulenKreisStreifenDiagrammeZeichnen(werte=werte,typ=typ,titel=auswahl,streifen=streifen)
         if typ=='ZeichnenUndEinfaerben':
-            afg = afg + [F'Zeichne das Säulendiagramm und färbe das Kreisdiagramm korrekt ein.\\\\ ']
+            afg = afg + [F'Zeichne das Säulendiagramm und färbe das {"Streifen" if streifen else "Kreis"}diagramm korrekt ein.\\\\ ']
             afg = afg + [F'Inhalt der Daten: {auswahl}\\\\']
-            afg = afg + saeulenDiagrammeZeichnen(werte=werte,typ=typ,titel=auswahl)
+            afg = afg + saeulenKreisStreifenDiagrammeZeichnen(werte=werte,typ=typ,titel=auswahl,streifen=streifen)
         if typ=='Zeichnen':
-            afg = afg + [F'Zeichne das Säulen und das Kreisdiagramm.\\\\ ']
+            afg = afg + [F'Zeichne das Säulen und das {"Streifen" if streifen else "Kreis"}diagramm.\\\\ ']
             afg = afg + [F'Inhalt der Daten: {auswahl}\\\\']
-            afg = afg + saeulenDiagrammeZeichnen(werte=werte,typ=typ,titel=auswahl)
+            afg = afg + saeulenKreisStreifenDiagrammeZeichnen(werte=werte,typ=typ,titel=auswahl,streifen=streifen)
         if typ=='ZeichnenUndBerechnen':
-            afg = afg + [F'Zeichne das Säulen und das Kreisdiagramm. Berechne dazu die Prozentwerte.\\\\ ']
+            afg = afg + [F'Zeichne das Säulen und das {"Streifen" if streifen else "Kreis"}diagramm. Berechne dazu die Prozentwerte.\\\\ ']
             afg = afg + [F'Inhalt der Daten: {auswahl}\\\\']
-            afg = afg + saeulenDiagrammeZeichnen(werte=werte,typ=typ,titel=auswahl)
+            afg = afg + saeulenKreisStreifenDiagrammeZeichnen(werte=werte,typ=typ,titel=auswahl,streifen=streifen)
         if typ=='alles':
-            afg = afg+ [F'Berechne die relative Häufigkeiten (Prozentwerte) folgender Daten und zeichne die absoluten Werte in ein Säulen und die relativen Werte in ein Kreisdiagramm.\\\\']
+            afg = afg+ [F'Berechne die relative Häufigkeiten (Prozentwerte) folgender Daten und zeichne die absoluten Werte in ein Säulen und die relativen Werte in ein {"Streifen" if streifen else "Kreis"}diagramm.\\\\']
             afg = afg + [F'Inhalt der Daten: {auswahl}\\\\']
-            afg = afg + saeulenDiagrammeZeichnen(werte=werte,typ=typ,titel=auswahl)
+            afg = afg + saeulenKreisStreifenDiagrammeZeichnen(werte=werte,typ=typ,titel=auswahl,streifen=streifen)
     afg=afg+['}']
-    lsg=saeulenDiagrammeZeichnen(werte=werte,typ='LSG',titel=auswahl)
+    lsg=saeulenKreisStreifenDiagrammeZeichnen(werte=werte,typ='LSG',titel=auswahl,streifen=streifen)
     return [afg,lsg,[]]
--- Funktionen/funktionenTikzDiagrammAufgabe.py
+++ Funktionen/funktionenTikzDiagrammAufgabe.py
@@ -6,7 +6,7 @@
 #Aufruf:
 #       exec(open("Funktionen/funktionen.py").read())
 
-def saeulenDiagrammeZeichnen(werte=[['VW','Mercedes','Audi','BMW','Honda',],[250,200,180,170,100]],titel='Autos pro Stunde',typ='LSG'):
+def saeulenKreisStreifenDiagrammeZeichnen(werte=[['VW','Mercedes','Audi','BMW','Honda',],[250,200,180,170,100]],titel='Autos pro Stunde',typ='LSG',streifen=False):
     zuordnen=True if (typ=='zuordnen') else False
     zeichnenEinfarben=True if (typ=='ZeichnenUndEinfaerben') else False
     zeichnen=True if (typ=='Zeichnen') else False
@@ -24,21 +24,40 @@ def saeulenDiagrammeZeichnen(werte=[['VW','Mercedes','Audi','BMW','Honda',],[250
     dia,hoehe=saeulenDiagrammTikzVorgBreiteHoehe(werte=werte,yAchse=[0,max(werte[1]),20],ylabel=titel,urspr=[0,0],mitUmrandung=False,gibHoeheZurueck=True,farben=farben,typ=typ)
     if not alles:
         tikzcommand=tikzcommand+dia
+    if streifen:
+        hoehe=hoehe+0.5
     tikzcommand=tikzcommand+tikzTabelle(tabelle=werte,dim=[2.0,0.5],newCBuchst='X',tabellenPos=[0,hoehe+3],mitUmrandung=False)
-    kreisUrsp=[10,hoehe-R]
-    if not alles:
-        tikzcommand.append(F'\\draw ({kreisUrsp[0]},{kreisUrsp[1]}) circle ({R} cm);')
-        tikzcommand.append(F'\\draw[fill=black] ({kreisUrsp[0]},{kreisUrsp[1]}) circle (0.05 cm);')
-    w=0
-    if not (zeichnen or zeichnenUndBerechnen or alles):
-        for i in range(len(pWerte)):
-            p=pWerte[i]
-            if zeichnenEinfarben:
-                tikzcommand.append(F'\\draw[thick] ({kreisUrsp[0]},{kreisUrsp[1]}) -- +({w}:{R}) arc ({w}:{w+p/100*360}:{R}) -- ({kreisUrsp[0]},{kreisUrsp[1]});')
-            else:
-                tikzcommand.append(F'\\draw[thick,pattern=north west lines, pattern color={farben[i]}] ({kreisUrsp[0]},{kreisUrsp[1]}) -- +({w}:{R}) arc ({w}:{w+p/100*360}:{R}) -- ({kreisUrsp[0]},{kreisUrsp[1]});')
-            if not (zuordnen or zeichnenEinfarben):
-                tikzcommand.append(F'\\node[rotate={w+p/100*360/2}] at ($({kreisUrsp[0]},{kreisUrsp[1]})+({w+p/100*360/2}:{R/2})$) {{\\textbf{{ {werte[0][i]} }} }};')
-            w=w+p/100*360
+    if streifen:
+        streifenUrsp=[0,hoehe]
+        streifenL=10
+        if not alles:
+            tikzcommand.append(F'\\draw[draw=black] ({streifenUrsp[0]},{streifenUrsp[1]}) rectangle ++(10,1);')
+        l = 0
+        if not (zeichnen or zeichnenUndBerechnen or alles):
+            for i in range(len(pWerte)):
+                p = pWerte[i]
+                if zeichnenEinfarben:
+                    tikzcommand.append(F'\\draw[thick] ({l},{streifenUrsp[1]}) rectangle ++({p/100*10},1);')
+                else:
+                    tikzcommand.append( F'\\draw[thick,pattern=north west lines, pattern color={farben[i]}] ({l},{streifenUrsp[1]}) rectangle ++({p/100*10},1);')
+                if not (zuordnen or zeichnenEinfarben):
+                    tikzcommand.append( F'\\node at ({l+p/100*10/2},{streifenUrsp[1] +0.5}) {{\\textbf{{ {werte[0][i]} }} }};')
+                l=l+p/100*10
+    else:
+        kreisUrsp=[10,hoehe-R]
+        if not alles:
+            tikzcommand.append(F'\\draw ({kreisUrsp[0]},{kreisUrsp[1]}) circle ({R} cm);')
+            tikzcommand.append(F'\\draw[fill=black] ({kreisUrsp[0]},{kreisUrsp[1]}) circle (0.05 cm);')
+        w=0
+        if not (zeichnen or zeichnenUndBerechnen or alles):
+            for i in range(len(pWerte)):
+                p=pWerte[i]
+                if zeichnenEinfarben:
+                    tikzcommand.append(F'\\draw[thick] ({kreisUrsp[0]},{kreisUrsp[1]}) -- +({w}:{R}) arc ({w}:{w+p/100*360}:{R}) -- ({kreisUrsp[0]},{kreisUrsp[1]});')
+                else:
+                    tikzcommand.append(F'\\draw[thick,pattern=north west lines, pattern color={farben[i]}] ({kreisUrsp[0]},{kreisUrsp[1]}) -- +({w}:{R}) arc ({w}:{w+p/100*360}:{R}) -- ({kreisUrsp[0]},{kreisUrsp[1]});')
+                if not (zuordnen or zeichnenEinfarben):
+                    tikzcommand.append(F'\\node[rotate={w+p/100*360/2}] at ($({kreisUrsp[0]},{kreisUrsp[1]})+({w+p/100*360/2}:{R/2})$) {{\\textbf{{ {werte[0][i]} }} }};')
+                w=w+p/100*360
     tikzcommand.append('\\end{tikzpicture}')
     return tikzcommand
