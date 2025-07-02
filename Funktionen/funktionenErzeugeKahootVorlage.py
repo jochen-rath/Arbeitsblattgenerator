62f3ae69c58ecb33be239423b3d29aca62494d0b
jochen-rath <jochen.rath@hotmail.de>
2025-07-02 22:52:13 +0200

    Kahoot: Weiter Terme und Gleichungen auch in Schön.

--- Funktionen/funktionenErzeugeKahootVorlage.py
+++ Funktionen/funktionenErzeugeKahootVorlage.py
@@ -91,27 +91,33 @@ def erzeugeKahootFachwoerter(zeit=10,art=''):
     return [afg] + results  + [zeit] + [results.index(ges)+1]
     
 
-def erzeugeKahootTermeEinfachEinsetzen(zeit=10,HS=False,EQ=False):
+def erzeugeKahootTermeEinfachEinsetzen(zeit=10,HS=False,formelSchoen=False):
+    alleVaris=['x','y','z','a','b']
+    vari=random.choice(alleVaris)
     op=random.choice(['+','-','*','/'])    
     x=random.randint(1,100) if op in ['+','-'] else random.randint(1,15)
-    zahl=random.randint(1,100) if op in ['+','-'] else random.randint(1,15)     
-    term=f'x{op}{zahl}'
-    erg=eval(term.replace('x',str(x)))
+    zahl=random.randint(1,100) if op in ['+','-'] else random.randint(1,15)
+    variZuerst=bool(random.getrandbits(1)) or op=='/'
+    term=f'{vari}{op}{zahl}' if variZuerst else f'{zahl}{op}{vari}'
+    erg=eval(term.replace(f'{vari}',str(x)))
     if HS:
         while erg<0:
             x=random.randint(1,100) if op in ['+','-'] else random.randint(1,15)
             zahl=random.randint(1,100) if op in ['+','-'] else random.randint(1,15)     
-            term=f'x{op}{zahl}'
-            erg=eval(term.replace('x',str(x)))
-    if  op=='/':
-        term=f'\\frac{{x}}{{{zahl}}}' if EQ else term
-        x,erg=x*zahl,x
+            term=f'{zahl}{op}{vari}' if variZuerst else f'{vari}{op}{zahl}'
+            erg=eval(term.replace(f'{vari}',str(x)))
+    if  op=='/' and formelSchoen:
+        term=f'\\frac{{{vari}}}{{{zahl}}}'
+    x,erg=(x*zahl,x) if op=='/' else (x,erg)
     auswahl=random.sample(range(1, 5 if erg<10 else 10), 3)
     results=[erg,erg+(1 if bool(random.getrandbits(1)) else -1)*auswahl[0],erg+(1 if bool(random.getrandbits(1)) else -1)*auswahl[1],erg+(1 if bool(random.getrandbits(1)) else -1)*auswahl[2]]
     random.shuffle(results)
-    return [f'{"$$" if EQ else ""}x={x} → {term.replace("*","·").replace("/",":")}=?{"$$" if EQ else ""}']+results+[zeit,results.index(erg)+1]
+    frage=f'$${vari}={x}~\\rightarrow~{term.replace("*","·").replace("/",":")}=?$$' if formelSchoen else f'x={x} → {term.replace("*","·").replace("/",":")}=?'
+    return [frage]+results+[zeit,results.index(erg)+1]
 
-def erzeugeKahootTermeKombiEinsetzen(zeit=10,HS=False):
+def erzeugeKahootTermeKombiEinsetzen(zeit=10,HS=False,formelSchoen=False):
+    alleVaris=['x','y','z','a','b']
+    vari=random.choice(alleVaris)
     op=random.choice(['+','-'])
     op2=random.choice(['*','/'])
     Z1=random.randint(1,10 if HS else 14)
@@ -121,12 +127,13 @@ def erzeugeKahootTermeKombiEinsetzen(zeit=10,HS=False):
 #Keine negativen Ergebnisse für die HS
     erg=(1 if bool(random.getrandbits(1)) or HS else -1)*random.randint((ergTerm1+1) if HS and op=='+' else 1,  (ergTerm1+1) if HS and op=='-' else 150)
     Z2=(erg-ergTerm1) if op=='+' else (ergTerm1-erg)
-    term=f'{Z1}*x{op}{Z2}' if op2=='*' else f'x/{Z1}{op}{Z2}'
+    term=f'{Z1}*{vari}{op}{Z2}' if op2=='*' else (f'\\frac{{{vari}}}{{{Z1}}}{op}{Z2}' if formelSchoen else f'{vari}/{Z1}{op}{Z2}')
     term=term.replace('--','+').replace('+-','-')
     auswahl=random.sample(range(1, 5 if erg<10 else 10), 3)
     results=[erg,erg+(1 if bool(random.getrandbits(1)) else -1)*auswahl[0],erg+(1 if bool(random.getrandbits(1)) else -1)*auswahl[1],erg+(1 if bool(random.getrandbits(1)) else -1)*auswahl[2]]
     random.shuffle(results)
-    return [f'x={x} → {term.replace("*","·").replace("/",":")}=?']+results+[zeit,results.index(erg)+1]
+    frage=f'$${vari}={x}~\\rightarrow~{term.replace("*","·").replace("/",":")}=?$$' if formelSchoen else f'x={x} → {term.replace("*","·").replace("/",":")}=?'
+    return [frage]+results+[zeit,results.index(erg)+1]
 
 
 def erzeugeKahootTermeZusammenfassen(zeit=10,anzVari=1,anzKoeffProVari=2,HS=False,rekursion=False):
@@ -189,7 +196,7 @@ def erzeugeKahootTermeZusammenfassen(zeit=10,anzVari=1,anzKoeffProVari=2,HS=Fals
     return [f'Fasse zusammen: {term}']+results+[zeit,ergIndizes] if not rekursion else rekur
 
 
-def erzeugeKahootGleichungFehlendEintragen(zeit=10):
+def erzeugeKahootGleichungFehlendEintragen(zeit=10,formelSchoen=False):
     alleVaris=['x','y','z','a','b']
     op=random.choice(['+','-','·',':'])
     opGegen={'·':':',':':'·','+':'-','-':'+'}
@@ -200,12 +207,17 @@ def erzeugeKahootGleichungFehlendEintragen(zeit=10):
     e=list(einsetzen)
     auswahl=random.randint(0,len(einsetzen)-1)
     e[auswahl]='◻'
-    form=f'{f"{e[0]}{e[1]}{e[2]}" if not op=='·' else f"{e[2]}{e[1]}{e[0]}"} = {erg}  |  {e[3]}{e[4]}'
+    gl=f'{f"{e[0]}{e[1]}{e[2]}" if not op=='·' else f"{e[2]}{e[1]}{e[0]}"} = {erg}  |  {e[3]}{e[4]}'
+    if formelSchoen and op2==':': 
+        gl=f'\\frac{{x}}{{{e[2]}}} = {erg} |  {e[3]}{e[4]}'
+    if formelSchoen: 
+        gl=gl.replace('◻','\\textcolor{red}{\square}').replace('|', '\\quad\\mid\\quad')
     del e[auswahl]
     results=[einsetzen[auswahl]]+random.sample(e,3)
     random.shuffle(results)
     ergIndizes=','.join([str(i+1) for i, x in enumerate(results) if x == einsetzen[auswahl]])
-    return [f'Was fehlt: {form}']+results+[zeit,ergIndizes]
+    frage=f'$$\\text{{Was fehlt: \\quad}}{gl}$$' if formelSchoen else f'Was fehlt: {gl}'
+    return [frage]+results+[zeit,ergIndizes]
 
 def erzeugeKahootGleichungFehlendEintragenKombi(zeit=10,formelSchoen=False):
     alleVaris=['x','y','z','a','b']
@@ -241,14 +253,14 @@ def erzeugeKahootTabellenInhalt(anzahl=10,zeit=20,dateiName='newFile',datum='',f
         kahootFkt=['erzeugeKahootKopfrechnen(zeit=zeit)',
                    random.choice(['kahootAdditionSchaetzen(zeit=zeit)', 'kahootSubtraktionSchaetzen(zeit=zeit)']),
                    'erzeugeKahootFachwoerter(zeit=zeit)',
-                    'erzeugeKahootTermeEinfachEinsetzen(zeit=zeit,HS=True)',
-                    'erzeugeKahootTermeEinfachEinsetzen(zeit=zeit)',
-                    'erzeugeKahootTermeKombiEinsetzen(zeit=zeit,HS=True)',
-                    'erzeugeKahootTermeKombiEinsetzen(zeit=zeit)',
-                   'erzeugeKahootTermeZusammenfassen(zeit=zeit,HS=True)',
-                   'erzeugeKahootTermeZusammenfassen(zeit=zeit,HS=False)',
-                   'erzeugeKahootTermeZusammenfassen(zeit=zeit,anzVari=2,HS=False)',
-                   'erzeugeKahootGleichungFehlendEintragen(zeit=zeit)',
+                    f'erzeugeKahootTermeEinfachEinsetzen(zeit=zeit,HS=True,formelSchoen={formelSchoen})',
+                    f'erzeugeKahootTermeEinfachEinsetzen(zeit=zeit,formelSchoen={formelSchoen})',
+                    f'erzeugeKahootTermeKombiEinsetzen(zeit=zeit,HS=True,formelSchoen={formelSchoen})',
+                    f'erzeugeKahootTermeKombiEinsetzen(zeit=zeit,formelSchoen={formelSchoen})',
+                   f'erzeugeKahootTermeZusammenfassen(zeit=zeit,HS=True)',
+                   f'erzeugeKahootTermeZusammenfassen(zeit=zeit,HS=False)',
+                   f'erzeugeKahootTermeZusammenfassen(zeit=zeit,anzVari=2,HS=False)',
+                   f'erzeugeKahootGleichungFehlendEintragen(zeit=zeit)',
                    f'erzeugeKahootGleichungFehlendEintragenKombi(zeit=zeit,formelSchoen={formelSchoen})']
         auswahl=list(kahootFkt)[0:3]
         if typ=='Kopf':
