a2f22ae1b5f03e3436093ee48bcefd2219fae1de
jochen-rath <jochen.rath@hotmail.de>
2025-12-19 09:03:54 +0100

    Tangens und Gemischt Formulieren

--- Funktionen/funktionenErzeugeSinusSatzAufgaben.py
+++ Funktionen/funktionenErzeugeSinusSatzAufgaben.py
@@ -13,22 +13,22 @@ import math
 import random
 #    farben={"magenta":"Magenta","darkgray":"Dunkelgrau", "violet":"Violett","blue":"Blau","gray":"Grau","green":"Green","brown":"Braun","black":"Schwarz","pink":"Rosa","red":"Rot","yellow":"Gelb","orange":"Orange"}
 
-def erzeugeSinussatzRechtwBenennen(mitText=True,anzSpalten=[2,2]):
+def erzeugeSinussatzRechtwBenennen(sinKosTan='',mitText=True,anzSpalten=[2,2]):
     wBez=random.sample(list(abcZuGr.keys()),3)
     rechtWinkelNr=random.randint(0,2)
     seiten=list(wBez)
     rechterWinkelBei=wBez[rechtWinkelNr].upper()
     hyp=wBez[rechtWinkelNr].lower()
     del seiten[rechtWinkelNr]
-    gesucht=[random.choice(seiten),random.choice(['Sinus','Kosinus'])]
+    gesucht=[random.choice(seiten),random.choice(['Sinus','Kosinus','Tangens']) if len(sinKosTan)<1 else sinKosTan]
     gegKat=gesucht[0].lower()
     anKat=[x for x in seiten if not x==gegKat][0]
     afgText=f'Formuliere den {gesucht[1]} und markiere die benötigten Seiten zur Berechnung des roten Winkels von folgendem Dreieck\\\\'
     afg=['\\pbox{\\linewidth}{']+([afgText] if mitText else [f'{gesucht[1]}:\\\\'])
     afg=afg+planfigurRWSinusKosinus(rW=rechterWinkelBei,wBez=wBez,ges=gesucht[0])
     afg=afg+['}']
-    loesung={'Sinus':['\\sin','gegKat','hyp'],'Kosinus':['\\cos','anKat','hyp']}
+    loesung={'Sinus':['\\sin','gegKat','hyp'],'Kosinus':['\\cos','anKat','hyp'],'Tangens':['\\tan','gegKat','anKat']}
     lsg=['\\pbox{\\linewidth}{']+[f'${loesung[gesucht[1]][0]}({abcZuGr[gesucht[0]]})=\\left(\\frac{{{eval(loesung[gesucht[1]][1])}}}{{{eval(loesung[gesucht[1]][2])}}}\\right)$\\\\']
-    lsg=lsg+planfigurRWSinusKosinus(rW=rechterWinkelBei,wBez=wBez,ges=gesucht[0],markieren=[hyp,eval(loesung[gesucht[1]][1])])
+    lsg=lsg+planfigurRWSinusKosinus(rW=rechterWinkelBei,wBez=wBez,ges=gesucht[0],markieren=[eval(loesung[gesucht[1]][2]),eval(loesung[gesucht[1]][1])])
     lsg=lsg+['}']    
     return [afg,lsg,[]]
